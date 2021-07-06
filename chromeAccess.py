39292e1b780dfa4d1cd0362a7b46064ce3c8d401
Nick <lindn098@umn.edu>
2021-07-06 11:37:51 -0500

    Tested try except with find element

--- chromeAccess.py
+++ chromeAccess.py
@@ -6,9 +6,11 @@ import chromedriver_binary
 wd = wd.Chrome()
 wd.implicitly_wait(10)
 
-wd.get("https://www.bestbuy.com/site/just-dance-2021-playstation-5/6427617.p?skuId=6427617")
+wd.get("https://www.bestbuy.com/site/sony-playstation-5-console/6426149.p?skuId=6426149")
 
-cartButton = wd.find_element_by_xpath("/html/body/div[3]/main/div[2]/div[3]/div[2]/div/div/div[7]/div[1]/div/div/div/button")
+try:
+    cartButton = wd.find_element_by_xpath("/html/body/div[3]/main/div[2]/div[3]/div[2]/div/div/div[7]/div[1]/div/div/div/button")
 
-cartButton.click()
-
+    cartButton.click()
+except:
+    print("failed to find element")
