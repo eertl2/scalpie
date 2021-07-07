3cb6e4a77ee2849daf8ebb00200bba5d7584599e
Nick <lindn098@umn.edu>
2021-07-06 21:04:59 -0500

    testing for sold out

--- chromeAccess.py
+++ chromeAccess.py
@@ -6,11 +6,11 @@ import chromedriver_binary
 wd = wd.Chrome()
 wd.implicitly_wait(10)
 
-wd.get("https://www.bestbuy.com/site/sony-playstation-5-console/6426149.p?skuId=6426149")
+wd.get("https://www.bestbuy.com/site/sony-playstation-5-digital-edition-console/6430161.p?skuId=6430161")
 
 try:
-    cartButton = wd.find_element_by_xpath("/html/body/div[3]/main/div[2]/div[3]/div[2]/div/div/div[7]/div[1]/div/div/div/button")
-
-    cartButton.click()
+    cartButton = wd.find_element_by_class_name("add-to-cart-button")
+    print(cartButton.get_attribute("data-button-state"))
 except:
     print("failed to find element")
+
