fabadc8645d20699dbde3f43538106032ae4e0cc
Nick <lindn098@umn.edu>
2021-07-06 21:30:41 -0500

    fixed timing

--- chromeAccess.py
+++ chromeAccess.py
@@ -13,20 +13,16 @@ running = True
 #success = False
 
 while running:
-    try:
-        time.sleep(10)
-        wd.refresh()
-        cartbutton = wd.find_element_by_class_name("add-to-cart-button")
+    wd.refresh()
+    cartbutton = wd.find_element_by_class_name("add-to-cart-button")
 
-        print("element found...")
-        #cartButton.click()
-        if(cartbutton.get_attribute("data-button-state")) == "SOLD_OUT":
-            print("item is sold out, retrying in 10")
-            continue
-        break
-    except:
-        print("failed to find element...retrying in 10")
+    print("element found...")
+    #cartButton.click()
+    if(cartbutton.get_attribute("data-button-state")) == "SOLD_OUT":
+        print("item is sold out, retrying in 10")
         continue
+    break
+
 
 
 
