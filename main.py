60ef34b1564212b985a3b3a75bc5301d89cdaf94
eertl2 <eertl2@wisc.edu>
2021-07-09 14:02:47 -0500

    double log fix

--- main.py
+++ main.py
@@ -6,16 +6,15 @@ from worker import worker
 import threading
 import chromedriver_binary # type: ignore
 
-#Initialize debugger
-dbgr = dbg.dbgr()
-
 #Item list
 #wd.get("https://www.bestbuy.com/site/sony-playstation-5-console/6426149.p?skuId=6426149") #ps5
 item = "https://www.bestbuy.com/site/happiness-is-a-warm-blanket-charlie-brown-dvd-2011/2095286.p?skuId=2095286" #test-item (in-stock)
 
 threads = []
-for i in range(3):
+for i in range(2):
     t = threading.Thread(target=worker, args = ("bestbuy", item))
     threads.append(t)
     t.start()
+
+
    
