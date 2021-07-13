89cdc68949c2f295b2077394942d6023313b47ec
eertl2 <eertl2@wisc.edu>
2021-07-13 11:38:35 -0500

    moved item into glv.py (only temp)

--- glv.py
+++ glv.py
@@ -3,4 +3,7 @@ PRINT_TO_LOGS = True
 MAX_RETRYS_PER_TASK = 1
 MIN_REFRESH_TIME = 8
 MAX_REFRESH_TIME = 12
+
+#move into new py class
 QUANITY = 1
+ITEM = "https://www.bestbuy.com/site/happiness-is-a-warm-blanket-charlie-brown-dvd-2011/2095286.p?skuId=2095286" #test-item (in-stock)
