d26643abb1af1d8abe4487308541d39940f6b2e7
ibrecht <72045031+ibrecht@users.noreply.github.com>
2021-07-14 18:35:10 -0500

    inheritance

--- bestbuy.py
+++ bestbuy.py
@@ -5,53 +5,13 @@ import dbg
 import time
 import traceback
 import chromedriver_binary
+from company import company
 
-class bestbuy:
-    username = None
-    password = None
-    firstname = None
-    lastname = None
-    address = None
-    city = None
-    state = None
-    zipcode = None
-    card = None
-    expmonth = None
-    expyear = None
-    cvv = None
-    driver = None
-    dbgr = None
+class bestbuy(company):
+    pass
 
-    def __init__(self):
-        self.driver = wd.Chrome()
-        self.driver.implicitly_wait(15)
-        self.dbgr = dbg.dbgr()
 
-        #Parse user-details.txt
-        lines = []
-        userdata = []
-        with open("user-details.txt", "r") as f:
-            lines = f.readlines()
-
-        count = 0
-        for line in lines:
-            linestr = line.split('=')
-            userdata.append(linestr[1])
-            count += 1
-
-        #User-details
-        self.username = userdata[0]
-        self.password = userdata[1]
-        self.firstname = userdata[2]
-        self.lastname = userdata[3]
-        self.address = userdata[4]
-        self.city = userdata[5]
-        self.state = userdata[6]
-        self.zipcode = userdata[7]
-        self.card = userdata[8]
-        self.expmonth = userdata[9]
-        self.expyear = userdata[10]
-        self.cvv = userdata[11]
+        
     
     def purchase(self, link):
         self.driver.get(link)
