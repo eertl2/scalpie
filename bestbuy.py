3e954cad7fac5f38b484efbe234e07ba39b94bb1
eertl2 <eertl2@wisc.edu>
2021-07-16 13:48:20 -0500

    print screenshot on fail

--- bestbuy.py
+++ bestbuy.py
@@ -36,7 +36,8 @@ class bestbuy(company):
         except:
             dbg.debug("Program failed: " + traceback.format_exc())
             dbg.crash(self.driver.page_source)
-            dbg.screenshot(self.driver)
+            if glv.PRINT_SCREENSHOT:
+                dbg.screenshot(self.driver)
             self.driver.close
             raise
 
